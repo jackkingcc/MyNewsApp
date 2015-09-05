26c7aebc4c878b176aaadc1220b093f2d665d483
Gary <vicking88@sina.com>
2015-09-05 21:12:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/gary/mynewsapp/WelcomeActivity.java
@@ -0,0 +1,75 @@
+package com.example.gary.mynewsapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+public class WelcomeActivity extends AppCompatActivity {
+    private ImageView mImageView;
+    AlphaAnimation animation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        mImageView=(ImageView)findViewById(R.id.welcome);
+
+        animation = new AlphaAnimation(1,1.0f);
+        animation.setDuration(3 * 1000);
+        animation.setFillAfter(true);
+        mImageView.startAnimation(animation);
+
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                // TODO Auto-generated method stub
+
+            }
+            //动画结束监听
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                // TODO Auto-generated method stub
+//                Intent intent = new Intent(WellcomActivity.this,ContentActivity.class);
+//                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_welcome, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
